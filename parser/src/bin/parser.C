f3a7d3b38d34c253307bb67475cc5c6613ba9e58
Ami Gangui <ami@ganguli.com>
2013-03-20 22:25:14 -0400

    Start adding unit tests.

--- parser/src/bin/parser.C
+++ parser/src/bin/parser.C
@@ -2,6 +2,87 @@
 #include <iostream>
 #include <cstring>
 
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+
+TEST_CASE("keywordToId/round-trip", "Convert from keyword id to string and back again")
+{
+	for (PGParse::TokenId i = PGParse::INVALID; i < PGParse::KW_SENTINAL; i = PGParse::TokenId(i + 1)) {
+		PGParse::Token token(0, i);
+		const char *text = token.idString();
+		PGParse::TokenId id = PGParse::Token::keywordToId(text);
+		REQUIRE (id == i);
+	}
+}
+
+TEST_CASE("keywordToId/case-insensitive", "SQL keywords are not case-sensitive")
+{
+	REQUIRE (PGParse::Token::keywordToId("aCcEss") == PGParse::ACCESS_KW);
+}
+
+
+TEST_CASE("Scanner::scan/c-comments1", "SQL c-style comments")
+{
+	const char *bytes = " /*comment 1*/  /* comment 2*/ ";
+	//                   0000000000111111111122222222223
+	//                   0123456789012345678901234567890
+	PGParse::Token correct[] = {
+		{0, PGParse::WHITESPACE_T},
+		{1, PGParse::COMMENT_T},
+		{14, PGParse::WHITESPACE_T},
+		{16, PGParse::COMMENT_T},
+		{30, PGParse::WHITESPACE_T},
+		{31, PGParse::INVALID}       // FIXME!!!
+	};
+	REQUIRE (true);
+	PGParse::Scanner scanner;
+	std::size_t len = strlen(bytes);
+	scanner.scan(bytes, len+1);
+	int j = 0;
+	for (
+		PGParse::TokenList::const_iterator i = scanner.tokensBegin(); 
+		i != scanner.tokensEnd(); 
+		i ++, j ++
+	) {
+		//std::cout << i->offset() << std::endl;
+		REQUIRE(j < 6);
+		REQUIRE(i->offset() == correct[j].offset());
+		REQUIRE(i->id() == correct[j].id());
+	}
+}
+
+TEST_CASE("Scanner::scan/quote1", "Simple single quotes.")
+{
+	const char *bytes = " 'hello world' ";
+	//                   0000000000111111111122222222223
+	//                   0123456789012345678901234567890
+	PGParse::Token correct[] = {
+		{0, PGParse::WHITESPACE_T},
+		{1, PGParse::STRING_T},
+		{14, PGParse::WHITESPACE_T},
+		{15, PGParse::INVALID}       // FIXME!!!
+	};
+	REQUIRE (true);
+	PGParse::Scanner scanner;
+	std::size_t len = strlen(bytes);
+	scanner.scan(bytes, len+1);
+	int j = 0;
+	for (
+		PGParse::TokenList::const_iterator i = scanner.tokensBegin(); 
+		i != scanner.tokensEnd(); 
+		i ++, j ++
+	) {
+		//std::cout << i->offset() << std::endl;
+		REQUIRE(j < 5);
+		REQUIRE(i->offset() == correct[j].offset());
+		REQUIRE(i->id() == correct[j].id());
+	}
+}
+
+
+#if 0
+
 int
 main(int argc, char **argv)
 {
@@ -38,3 +119,5 @@ main(int argc, char **argv)
 	
 	return 0;
 }
+
+#endif
