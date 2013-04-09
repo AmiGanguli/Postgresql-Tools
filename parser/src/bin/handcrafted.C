e050ef6d6b77782ce9e0ef6bde8279170bc6e978
Ami Gangui <ami@ganguli.com>
2013-04-09 09:30:19 -0400

    Parse 'drop table'.

--- parser/src/bin/handcrafted.C
+++ parser/src/bin/handcrafted.C
@@ -2,8 +2,19 @@
 #include <iostream>
 #include <cstring>
 #include <list>
+#include <boost/bind.hpp>
 
 namespace PGParse {
+	
+class Statement
+{
+};
+
+class DropTableStatement : public Statement
+{
+public:
+	DropTableStatement(TokenList::const_iterator table_name)	{}
+};
 	
 /**
  * A really simple recursive descent parser that takes advantage 
@@ -76,18 +87,62 @@ public:
 		return true;
 	}
 	
+	template <
+		class ITEM,
+		bool (ITEM::*FUNC)(ITEM**) 
+	>
 	bool
-	drop_table_statement()
+	zero_or_more_of(std::list<ITEM*>& out)
+	{
+		ITEM *item = 0;
+		while(boost::bind(FUNC, this, &item)) {
+			out.push_back(item);
+		}
+		// Always successful, since an empty sequence is valid.
+		return true;
+	}
+
+	template <
+		class ITEM,
+		bool (ITEM::*FUNC)(ITEM**) 
+	>
+	bool
+	one_or_more_of(std::list<ITEM*>& out)
+	{
+		ITEM *item = 0;
+		while(boost::bind(FUNC, this, &item)) {
+			out.push_back(item);
+		}
+		// Item still holds the last item found.
+		return item != 0;
+	}
+	
+	bool
+	drop_table(Statement **out)
 	{
 		iterator identifier;
 		if (
 			token(DROP_KW)
 			&& token(TABLE_KW)
 			&& token(IDENTIFIER_T, &identifier)
-			&& token(SEMI_COLON_T)
 		) {
 			std::cout << "identifier " << identifier->idString() << std::endl;
+			*out = new DropTableStatement(identifier);
 			return true;
+		}
+		return false;
+	}
+	
+	bool
+	statement(Statement** out)
+	{
+		Statement *out_ = 0;
+		if (drop_table(&out_)) {
+			if (token(SEMI_COLON_T)) {
+				*out = out_;
+				return true;
+			}
+			delete out_;
 		}
 		return false;
 	}
@@ -99,9 +154,11 @@ parse(
 	const TokenList::const_iterator& end
 )
 {
+	Statement *statement;
 	Parser parser(begin, end);
-	if (parser.drop_table_statement()) {
+	if (parser.statement(&statement)) {
 		std::cout << "match!" << std::endl;
+		delete statement;
 	} else {
 		std::cout << "fail!" << std::endl;
 	}
@@ -117,7 +174,7 @@ parse(
 
 int main()
 {
-	const char *bytes = "Drop /* C-style comment*/ 	 Table a_table_name;";
+	const char *bytes = "Drop /* C-style comment*/  Table a_table_name;";
 	
 	PGParse::Scanner scanner;
 	std::size_t len = strlen(bytes);
